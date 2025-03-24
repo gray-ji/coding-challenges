e13943d84a626e6540f392fef52d5fe31f856958
Hyeon <149739527+gray-ji@users.noreply.github.com>
2025-03-24 17:57:44 +0900

    [level 0] Title: 수 조작하기 2, Time: 19.70 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,8 +1,8 @@
 def solution(numLog):
-    control_dic = { -1: 'w', 1: 's', -10: 'd', 10: 'a' }
     answer = ''
+    control_dic = { 1: 'w', -1: 's', 10: 'd', -10: 'a' }
     
     for i in range(1, len(numLog)):
-        answer += control_dic[numLog[i - 1] - numLog[i]]
+        answer += control_dic[numLog[i] - numLog[i - 1]]
         
     return answer
