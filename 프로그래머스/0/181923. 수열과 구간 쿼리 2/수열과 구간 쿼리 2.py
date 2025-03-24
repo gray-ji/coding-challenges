aaa8067c1724045ec0e4f37ae81f31cc51d3f2fc
Hyeon <149739527+gray-ji@users.noreply.github.com>
2025-03-24 18:22:10 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 12.22 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -4,6 +4,6 @@ def solution(arr, queries):
     for s, e, k in queries:
         answer_part = [arr[i] for i in range(s, e + 1) if arr[i] > k]
         
-        answer.append(-1 if len(answer_part) == 0 else min(answer_part))
+        answer.append(-1 if not answer_part else min(answer_part))
             
     return answer
