58c1a2d128db29f593595b4f3e828e52be9800ad
Hyeon <149739527+gray-ji@users.noreply.github.com>
2025-04-02 16:19:23 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 57.83 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,9 +1,4 @@
 def solution(arr, k):
     if k % 2 == 0:
-        for i in range(len(arr)):
-            arr[i] += k
-    else:
-        for i in range(len(arr)):
-            arr[i] *= k
-    
-    return arr
+        return [a + k for a in arr]
+    return [a * k for a in arr]
