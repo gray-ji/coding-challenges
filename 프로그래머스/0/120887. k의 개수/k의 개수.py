5cb36ec23d3e990192459f2bc3801a69c9467084
Hyeon <149739527+gray-ji@users.noreply.github.com>
2025-04-30 00:49:35 +0900

    [level 0] Title: k의 개수, Time: 81.91 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -0,0 +1,9 @@
+def solution(i, j, k):
+    answer = 0
+    
+    for num in range(i, j+1):
+        for n in str(num):
+            if k == int(n):
+                answer += 1
+    
+    return answer
