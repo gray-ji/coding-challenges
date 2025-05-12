2b41baf0e72dd2a01e9adb020ccf40d377c51fb8
Hyeon <149739527+gray-ji@users.noreply.github.com>
2025-05-12 18:07:42 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    
+    for _ in range(n):
+        answer += 1
+        
+        while True:
+            if answer % 3 and '3' not in str(answer):
+                break
+            answer += 1
+    
+    return answer
