2906995a9aa6353295abe0bec7a60b31927f8ab3
Hyeon <149739527+gray-ji@users.noreply.github.com>
2025-05-12 18:30:06 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,17 @@
+def solution(score):
+    answer = [0] * len(score)
+
+    avgs = [[sum(v) / 2, i] for i, v in enumerate(score)]
+    avgs.sort(reverse = True)
+    
+    rank, dup = 1, 1
+    for i, v in enumerate(avgs):
+        answer[v[1]] = rank
+        
+        if (i + 1) < len(avgs) and avgs[i+1][0] == v[0]:
+            dup += 1
+        else:
+            rank += dup
+            dup = 1
+    
+    return answer
