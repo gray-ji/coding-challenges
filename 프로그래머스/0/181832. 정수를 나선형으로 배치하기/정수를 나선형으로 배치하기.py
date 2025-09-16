6e93863e3d4d9cbd1052f8893dd7ff0066eefd8c
Hyeon <149739527+gray-ji@users.noreply.github.com>
2025-09-16 12:52:12 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.13 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,36 +1,28 @@
 def solution(n):
-    if n == 1:
-        return [[1]]
-    
     answer = [[0] * n for _ in range(n)]
-    numbers = [(i * n) + j for i in range(n) for j in range(1, n+1)]
-
-    i, j = 0, 0
-    dir = 'R'
-    s_cnt = {'R': 0, 'D': 0, 'L': 0, 'U': 0}
+	
+    top, bottom, left, right = 0, n-1, 0, n-1
+    num = 1
     
-    while answer[i][j] == 0:
-        answer[i][j] = numbers.pop(0)
-
-        if dir == 'R':
-            if j == (n - 2) - s_cnt['R']:
-                dir = 'D'
-                s_cnt['U'] += 1
-            j += 1
-        elif dir == 'D':
-            if i == (n - 2) - s_cnt['D']:
-                dir = 'L'
-                s_cnt['R'] += 1
-            i += 1
-        elif dir == 'L':
-            if j == 1 + s_cnt['L']:
-                dir = 'U'
-                s_cnt['D'] += 1
-            j -= 1
-        else:
-            if i == 1 + s_cnt['U']:
-                dir = 'R'
-                s_cnt['L'] += 1
-            i -= 1
+    while num <= n*n:
+        for j in range(left, right+1):
+            answer[top][j] = num
+            num += 1
+        top += 1
+        
+        for i in range(top, bottom+1):
+            answer[i][right] = num
+            num += 1
+        right -= 1
+        
+        for j in range(right, left-1, -1):
+            answer[bottom][j] = num
+            num += 1
+        bottom -= 1
+        
+        for i in range(bottom, top-1, -1):
+            answer[i][left] = num
+            num += 1
+        left += 1
     
     return answer
