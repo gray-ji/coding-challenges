64d11a05b7d15e6547317628a3d40266ae8b585f
Hyeon <149739527+gray-ji@users.noreply.github.com>
2026-01-03 14:30:55 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -2,9 +2,8 @@ def solution(numlist, n):
     tmp = [[0, num] for num in numlist]
     
     for i, v in enumerate(tmp):
+        tmp[i][0] = abs(n - v[1])
         if v[1] < n:
-            tmp[i][0] = (n - tmp[i][1]) + 0.5
-        else:
-            tmp[i][0] = tmp[i][1] - n
+            tmp[i][0] += 0.5
     
     return [num[1] for num in sorted(tmp)]
