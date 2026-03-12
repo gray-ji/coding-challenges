176a34eb781ec3514222287b73a9c7f2cdb4c812
Hyeon <149739527+gray-ji@users.noreply.github.com>
2026-03-12 23:56:01 +0900

    [level 2] Title: 숫자의 표현, Time: 1.65 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,16 @@
 def solution(n):
-    answer = 1
+    answer = 0
+    start, end = 1, 1
+    hab = 1
     
-    for i in range(1, n):
-        hab = i
-        for j in range(i+1, n):
-            hab += j
-            if hab >= n:
-                if hab == n:
-                    answer += 1
-                break
+    while start <= n:
+        if hab >= n:
+            if hab == n:
+                answer += 1
+            hab -= start
+            start += 1
+        else:
+            end += 1
+            hab += end
     
     return answer
