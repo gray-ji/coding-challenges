bd81ad581d06a714aed375a5c6e199bc2ebf4fa0
Hyeon <149739527+gray-ji@users.noreply.github.com>
2026-03-12 19:21:58 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.45 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,7 +1,2 @@
 def solution(A,B):
-    answer = 0
-
-    for x, y in zip(sorted(A), sorted(B, reverse=True)):
-        answer += x * y
-
-    return answer
+    return sum([a * b for a, b in zip(sorted(A), sorted(B, reverse=True))])
