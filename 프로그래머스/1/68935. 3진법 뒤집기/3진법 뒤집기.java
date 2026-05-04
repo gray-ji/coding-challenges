3982cf4ab6ef0b6bafc0886de29d179ea4d3fc1a
Hyeon <149739527+gray-ji@users.noreply.github.com>
2026-05-04 11:42:42 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,13 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        String sn = "";
+        int answer = 0;
+        ArrayList<Integer> nList = new ArrayList<>();
         
         while (n > 0) {
-            sn += n % 3;
+            nList.add(n % 3);
             n /= 3;
         }
         
-        return Integer.parseInt(sn, 3);
+        int idx = nList.size() - 1;
+        for (int i=0; i<=idx; i++) {
+            answer += nList.get(idx - i) * Math.pow(3, i);
+        }
+        
+        return answer;
     }
 }
